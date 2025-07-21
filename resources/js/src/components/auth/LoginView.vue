7766b15c01c9afeb5554454cdcab32af05faf402
HectorzBarz <hectorrodriguezbar99@gmail.com>
2025-07-21 10:49:16 +0200

    feature: Fixed Login and Register responsive design

--- resources/js/src/components/auth/LoginView.vue
+++ resources/js/src/components/auth/LoginView.vue
@@ -1,18 +1,18 @@
 <script setup></script>
 
 <template>
-    <section class="h-full w-full grid-cols-2 p-32">
+    <section class="h-full w-full px-5 py-32 xl:p-32">
         <div
-            class="grid grid-cols-2 rounded-xl border bg-violet-100 px-5 py-10"
+            class="grid rounded-l-sm rounded-r-sm bg-violet-100 px-5 py-10 lg:grid-cols-2 xl:rounded-xl"
         >
             <!-- bloque del formualario a rellenar -->
             <div
-                class="items-base grid border-r-2 border-violet-200 px-10 py-5"
+                class="items-base py-5 lg:border-r-2 lg:border-violet-200 lg:px-10"
             >
                 <form name="login-form">
                     <!-- input de email -->
                     <div class="mb-10 flex flex-col">
-                        <label for="username" class="text-2xl"
+                        <label for="email" class="text-2xl"
                             >Correo electrónico:
                         </label>
                         <input
@@ -20,6 +20,8 @@
                             type="email"
                             class="h-12 rounded-sm border bg-white px-2 text-xl"
                             placeholder="Introduce tu correo electrónico..."
+                            autocomplete="email"
+                            required
                         />
                     </div>
                     <!-- FIN input de email -->
@@ -41,7 +43,7 @@
                     <div class="flex items-center justify-center">
                         <!-- botón login -->
                         <button
-                            class="mb-2 w-[50%] cursor-pointer rounded-sm border bg-violet-200 px-2.5 py-1 text-2xl transition-colors hover:bg-violet-300"
+                            class="mb-2 cursor-pointer rounded-sm border bg-violet-200 px-2.5 py-1 text-2xl transition-colors hover:bg-violet-300 lg:w-[50%]"
                             type="submit"
                         >
                             Iniciar sesión
@@ -52,16 +54,16 @@
                     <div>
                         <div class="my-4 flex items-center justify-center">
                             <div class="h-px w-[20%] bg-black"></div>
-                            <span class="mx-4 text-xl font-bold"
+                            <span class="mx-4 text-center text-xl font-bold"
                                 >¿No tienes cuenta?</span
                             >
                             <div class="h-px w-[20%] bg-black"></div>
                         </div>
 
-                        <div class="flex items-center justify-center">
+                        <div class="flex justify-center">
                             <!-- botón de redirección a registro -->
                             <button
-                                class="mb-2 w-[50%] cursor-pointer rounded-sm border bg-violet-300 px-2.5 py-1 text-2xl transition-colors hover:bg-violet-400"
+                                class="mb-2 cursor-pointer rounded-sm border bg-violet-300 px-2.5 py-1 text-2xl transition-colors hover:bg-violet-400 lg:w-[50%]"
                             >
                                 <RouterLink to="/register">
                                     ¡Regístrate!
@@ -75,7 +77,9 @@
             <!-- FIN bloque del formualario a rellenar -->
 
             <!-- bloque de la imagén de inicio de sesión -->
-            <div class="flex h-full w-full items-center justify-center">
+            <div
+                class="hidden h-full w-full items-center justify-center lg:flex"
+            >
                 <img
                     src="/public/images/auth/roman_colosseum.png"
                     alt="auth-image"
--- resources/js/src/components/auth/RegisterView.vue
+++ resources/js/src/components/auth/RegisterView.vue
@@ -1,13 +1,13 @@
 <script setup></script>
 
 <template>
-    <section class="h-full w-full p-32">
+    <section class="h-full w-full px-5 py-32 xl:p-32">
         <div
-            class="grid grid-cols-2 rounded-xl border bg-violet-100 px-5 py-10"
+            class="grid rounded-l-sm rounded-r-sm bg-violet-100 px-5 py-10 lg:grid-cols-2 xl:rounded-xl"
         >
             <!-- bloque del formualario a rellenar -->
             <div
-                class="items-base grid border-r-2 border-violet-200 px-10 py-5"
+                class="items-base py-5 lg:border-r-2 lg:border-violet-200 lg:px-10"
             >
                 <form name="login-form">
                     <!-- input de username -->
@@ -18,20 +18,24 @@
                         <input
                             id="username"
                             type="username"
-                            class="h-12 border bg-white px-2 text-xl"
+                            class="h-12 rounded-sm border bg-white px-2 text-xl"
                             placeholder="Introduce tu nombre de usuario..."
+                            required
+                            autocomplete
                         />
                     </div>
                     <!-- input de username -->
                     <div class="mb-10 flex flex-col">
-                        <label for="username" class="text-2xl"
+                        <label for="email" class="text-2xl"
                             >Correo electrónico:
                         </label>
                         <input
                             id="email"
                             type="email"
-                            class="h-12 border bg-white px-2 text-xl"
+                            class="h-12 rounded-sm border bg-white px-2 text-xl"
                             placeholder="Introduce tu correo electrónico..."
+                            required
+                            autocomplete
                         />
                     </div>
                     <!-- FIN input de email -->
@@ -44,36 +48,37 @@
                         <input
                             id="password"
                             type="password"
-                            class="h-12 border bg-white px-2 text-xl"
+                            class="h-12 rounded-sm border bg-white px-2 text-xl"
                             placeholder="Introduce tu contraseña..."
+                            required
                         />
                     </div>
                     <!-- FIN input de password -->
 
                     <div class="flex items-center justify-center">
-                        <!-- botón login -->
+                        <!-- botón register -->
                         <button
-                            class="mb-2 w-[50%] cursor-pointer rounded-sm border bg-violet-200 px-2.5 py-1 text-2xl transition-colors hover:bg-violet-300"
+                            class="mb-2 cursor-pointer rounded-sm border bg-violet-200 px-2.5 py-1 text-2xl transition-colors hover:bg-violet-300 lg:w-[50%]"
                             type="submit"
                         >
                             Regístrarse
                         </button>
-                        <!-- FIN botón login -->
+                        <!-- FIN botón register -->
                     </div>
 
                     <div>
                         <div class="my-4 flex items-center justify-center">
                             <div class="h-px w-[20%] bg-black"></div>
-                            <span class="mx-4 text-xl font-bold"
+                            <span class="mx-4 text-center text-xl font-bold"
                                 >¿Ya tienes cuenta?</span
                             >
                             <div class="h-px w-[20%] bg-black"></div>
                         </div>
 
-                        <div class="flex items-center justify-center">
+                        <div class="flex justify-center">
                             <!-- botón de redirección a login -->
                             <button
-                                class="mb-2 w-[50%] cursor-pointer rounded-sm border bg-violet-300 px-2.5 py-1 text-2xl transition-colors hover:bg-violet-400"
+                                class="mb-2 cursor-pointer rounded-sm border bg-violet-300 px-2.5 py-1 text-2xl transition-colors hover:bg-violet-400 lg:w-[50%]"
                             >
                                 <RouterLink to="/login">
                                     ¡Inicia sesión!
@@ -87,7 +92,9 @@
             <!-- FIN bloque del formualario a rellenar -->
 
             <!-- bloque de la imagén de inicio de sesión -->
-            <div class="flex h-full items-center justify-center">
+            <div
+                class="hidden h-full w-full items-center justify-center lg:flex"
+            >
                 <img
                     src="/public/images/auth/pisa_tower.png"
                     alt="auth-image"
