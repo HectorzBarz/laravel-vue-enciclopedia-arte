a1bc41d2b35d52cfd64922973d4cad5a62e149cd
HectorzBarz <hectorrodriguezbar99@gmail.com>
2025-07-21 11:27:41 +0200

    feature: Installed PrimeVue and PrimeIcons && Configured Primevue to have tailwindcss priority over styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,28 @@
 import { createApp } from "vue";
 import App from "./src/App.vue";
 import router from "./src/router";
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
+import Aura from "@primeuix/themes/aura";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(PrimeVue, {
+    theme: {
+        preset: Aura,
+        options: {
+            darkModeSelector: ".fake-dark-selector", // trying to also force a non-usage of the dark mode
+            cssLayer: {
+                name: "primevue",
+                // Enable PrimeVue CSS layer and configure the tailwind styles to have higher specificity with layering
+                order: "tailwind-base, primevue, tailwind-utilities",
+            },
+        },
+    },
+});
+
+app.use(router);
+
+app.component("Button", Button);
+
+app.mount("#app");
--- resources/js/src/components/feedback/ContactView.vue
+++ resources/js/src/components/feedback/ContactView.vue
@@ -1,1 +1,11 @@
-<template>hola desde contact us view</template>
+<script></script>
+
+<template>
+    <Button label="Verify" class="border" />
+    <i class="pi pi-check" style="font-size: 1rem"></i>
+    <i class="pi pi-times" style="font-size: 1.5rem"></i>
+    <i class="pi pi-search" style="font-size: 2rem"></i>
+    <i class="pi pi-user" style="font-size: 2.5rem"></i>
+    <br />
+    hola desde contact us view
+</template>
