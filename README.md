fdea4634288f162e9d8acb27de676a5228fc9854
HectorzBarz <hectorrodriguezbar99@gmail.com>
2025-07-31 08:26:17 +0200

    quickfix: Deleted some unnecessary images and added a new register to MovementSeeder

--- README.md
+++ README.md
@@ -6,3 +6,30 @@ my programming and designing skills
 ## Changes Done
 
 -- Artist View now shows the artist movements
+
+## Working
+
+- HomePage, mostrar los mas recientes (orden descenciente)
+
+- BBDD, Crear Seeder para Artist-Movements
+
+## To-Do
+
+- BBDD, Crear Seeder para Artist-Art-Piece
+- BBDD, Crear Seeder para Tags
+- BBDD, Crear Seeder para Art-Piece-Tags
+
+- HomePage, Hacer la frase de dedicatoría de la página inicial
+
+- Obras, Mostrar en las obras el artista correspondiente
+
+- Artistas, Mostrar en la página de artistas algunas de sus obras
+
+- Movimientos, Mostrar en los movimientos sus obras representativas
+
+- Búsqueda, Mostrar todos los resultados en la página de búsqueda
+- Búsqueda, Filtrado de la página de búsqueda
+
+- Auth, Creación de usuarios con el sistema de Register/Login
+
+- PieceTags, Crear los tags de las piezas correspondientes
--- database/seeders/MovementSeeder.php
+++ database/seeders/MovementSeeder.php
@@ -35,6 +35,13 @@ class MovementSeeder extends Seeder
                 'end' => 1882,
                 'img' => 'movements/paseo-por-el-acantilado-de-pourville.jpg',
             ],
+            [
+                'title' => 'Manierismo',
+                'description' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque lacinia, enim quis vestibulum euismod, neque nibh commodo nulla, a imperdiet ligula neque non sem. Phasellus id justo faucibus, posuere nisi quis, sodales nisl. Etiam vel sem ullamcorper, mattis neque et, laoreet leo. Nullam ac diam eget nisl pharetra vulputate. Suspendisse non tincidunt justo. Maecenas eget massa fermentum, tristique sem in, feugiat urna. Ut sit amet nisi nec ex vehicula vulputate ullamcorper nec leo. In condimentum tempor velit, sed cursus odio consequat quis. Integer sagittis diam id pulvinar vulputate. Nam volutpat nibh non dui consectetur tincidunt. Sed vitae sodales nunc.',
+                'start' => 1550,
+                'end' => 1610,
+                'img' => 'movements/cristo-cargando-cruz-greco.jpg',
+            ],
         ];
 
 
